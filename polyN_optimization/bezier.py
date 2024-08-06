f80959abd91b20eca62dba1c64100f54e64d0e7e
Clément Nober <clement.nober@etu.minesparis.psl.eu>
2024-08-06 17:33:01 +0200

    plots

--- polyN_optimization/bezier.py
+++ polyN_optimization/bezier.py
@@ -19,6 +19,7 @@ c53 = binom(5,3)
 c54 = binom(5,4)
 c55 = binom(5,5)
 
+print(c51)
 def phi0(t):
     return c50 * np.power(1 - t,5)
 
@@ -45,16 +46,17 @@ def bezier_seg(bs, be, us, ue, ls, le, n):
     b4 = be - le * ue
     b5 = be
 
-
     def y(t):
         p = phi0(t) * b0 + phi1(t) * b1 + phi2(t) * b2 + phi3(t) * b3 + phi4(t) * b4 + phi5(t) * b5
         return(p)
 
     T = np.linspace(0, 1, n)
     Y = np.array([y(t) for t in T])
+
     return(Y)
 
 def param_data_dir(material):
+
     df = readData_2d(material, "bezier")
     data_dir = df[df["Type"] == "e"][df["q"] == 0]
     
@@ -96,10 +98,12 @@ def param_data_dir(material):
 
         v_ys = (1 - mu) * (ys2 - ys1) + mu * (ys3 - ys2)
         t_ys = v_ys/np.linalg.norm(v_ys)
+
         mus_ys[i] = t_ys
 
         v_r = (1 - mu) * (r2 - r1) + mu * (r3 - r2)
         t_r = v_r/np.linalg.norm(v_r)
+
         mus_r[i] = t_r
     
     mue_ys = np.roll(mus_ys, -1, axis=0)
@@ -118,7 +122,7 @@ def seg_data_dir(material, alpha):
     l_ys = alpha * lmax_ys
     l_r = alpha * lmax_r
     n_seg = len(mus_ys)
-    n = 1
+    n = 10
 
     B_ys = np.zeros((n * n_seg, 2))
     B_r = np.zeros((n * n_seg, 2))
@@ -163,16 +167,16 @@ def plot_seg_data_dir(material):
 
 def bound_lambda(bs, be, us, ue):
     v = np.dot(us, ue)
-    u = 1/(1 - v) * (be - bs)
+    u =(be - bs) /(1 - v) 
 
     t = np.dot(u, us - v * ue)
     tau = np.dot(u, ue - v * us)
 
     return(t/2, tau/2)
 
-def bezier_section_plane(material, theta, ys, ys_s, n_pt, r, r_s, r_tb=1, r_cb=1):
+
+def bezier_section_plane(material, theta, ys, ys_s, n_pt, r, r_s, r_tb=1, r_cb=1, tc_sym=1):
     theta_s = np.pi/2 - theta
-    print(theta, theta_s)
     df = readData_2d(material, "bezier")
     v = np.array([- np.sin(2 * theta), np.sin(2 * theta), np.cos(2 * theta)])
 
@@ -187,21 +191,23 @@ def bezier_section_plane(material, theta, ys, ys_s, n_pt, r, r_s, r_tb=1, r_cb=1
         b_t = sEBT * np.array([1/np.sqrt(2), 1/np.sqrt(2), 0]) / s0
 
     b_c = - b_t
+
     p_t = ys * np.array([np.square(np.cos(theta)), np.square(np.sin(theta)), np.sin(theta) * np.cos(theta)])
+    print(np.dot(p_t, v))
     p_ts = ys_s * np.array([np.square(np.sin(theta)), np.square(np.cos(theta)), - np.sin(theta) * np.cos(theta)])
 
     p_c = ys * np.array([- np.square(np.cos(theta)), - np.square(np.sin(theta)), - np.sin(theta) * np.cos(theta)])
     p_cs = ys_s * np.array([- np.square(np.sin(theta)), - np.square(np.cos(theta)), np.sin(theta) * np.cos(theta)])
 
     P = [b_t, p_ts, p_c, b_c, p_cs, p_t]
-
-
-    print(P)
     P_name = ["b_t", "p_ts", "p_c", "b_c", "p_cs", "p_t"]   
     B = np.zeros((n_pt * 6, 3))
 
     mus = np.zeros((6, 3))
     mue = np.zeros((6, 3))
+    ns = np.zeros((6, 3))
+    dgs = np.zeros((6, 3))
+    ws = np.zeros((6, 3)) 
 
     wm = np.array([r + np.square(np.sin(theta)), r + np.square(np.cos(theta)), - np.sin(theta) * np.cos(theta)])
     wp = np.array([r_s + np.square(np.sin(theta_s)), r_s + np.square(np.cos(theta_s)), np.sin(theta_s) * np.cos(theta_s)])
@@ -210,28 +216,36 @@ def bezier_section_plane(material, theta, ys, ys_s, n_pt, r, r_s, r_tb=1, r_cb=1
         p = P_name[i]
         if str(p) == "b_t":
             n = 1/np.sqrt(1 + np.square(r_tb)) * np.array([1, r_tb, 0])
+            dgs[i] = n
+            ws[i] = n
         elif str(p) == "b_c":
             n = 1/np.sqrt(1 + np.square(r_cb)) * np.array([-1, -r_cb, 0])
+            dgs[i] = n
+            ws[i] = n
         else :
             if str(p) == "p_t":
                 dg = np.array([- np.sin(2 * theta), np.sin(2 * theta), np.cos(2 * theta)])
                 w = wm
             elif str(p) == "p_ts":
-                dg = np.array([- np.sin(2 * theta_s), np.sin(2 * theta_s), np.cos(2 * theta_s)])
+                dg = np.array([np.sin(2 * theta), - np.sin(2 * theta), - np.cos(2 * theta)])
                 w = wp
             elif str(p) == "p_c":
                 dg = np.array([np.sin(2 * theta), - np.sin(2 * theta), - np.cos(2 * theta)])
                 w = wm
             elif str(p) == "p_cs":
-                dg = np.array([np.sin(2 * theta_s), - np.sin(2 * theta_s), - np.cos(2 * theta_s)])
+                dg = np.array([- np.sin(2 * theta), np.sin(2 * theta), np.cos(2 * theta)])
                 w = wp
             
             u = np.cross(w, dg)
             n = u/np.linalg.norm(u)
+            dgs[i] = dg
+            ws[i] = w
 
+        ns[i] = n
         u = np.cross(v, n)
         mus[i] = u/np.linalg.norm(u)
     
+    print(np.dot(mus, v))
     mue = np.roll(mus, -1, axis=0)
 
     l_max = np.zeros(6)
@@ -239,12 +253,21 @@ def bezier_section_plane(material, theta, ys, ys_s, n_pt, r, r_s, r_tb=1, r_cb=1
         halft, halftau = bound_lambda(P[i - 1], P[i], mus[i - 1], mue[i - 1])
         l_max[i - 1] = min(halft, halftau)
     
-    print(l_max)
-    ls = np.zeros(6)
+    L1 = l_max[0]
+    L2 = min(l_max[1], l_max[5])
+    L3 = min(l_max[2], l_max[4])
+    L4 = l_max[3]
+
+    if tc_sym:
+        L2 = min(L2, L3)
+        L3 = L2
+
+    ls = np.array([L1, L2, L3, L4, L3, L2]) * 0.5
+
     for i in range(6):
         B[i * n_pt:(i+1) * n_pt] = bezier_seg(P[i - 1], P[i], mus[i - 1], mue[i - 1], ls[i - 1], ls[i], n_pt)
     
-    return(B)
+    return(B, P, mus, mue, ns, dgs, ws, v)
 
 
 def plot_bezier(material):
@@ -260,16 +283,118 @@ def plot_bezier(material):
     n_data = len(B_ys)
     n_pt = 100
 
-    B = np.empty((1,3))
+    B = np.empty((0,3))
+
     for i in range(n_data):
-        B = np.concatenate((B, bezier_section_plane(material, thetas[i], ys[i], ys[n_data - i - 1], n_pt, rval[i], rval[n_data - i - 1])))
+        B_new, P_new, mus_new, mue_new, ns_new, dgs_new, ws_new = bezier_section_plane(material, thetas[i], ys[i], ys[n_data - i - 1], n_pt, rval[i], rval[n_data - i - 1])
+        B = np.concatenate((B, B_new))
 
     X = B[:,0]
     Y = B[:,1]
     Z = B[:,2]
 
-    ax.scatter(X, Y, Z, cmap='viridis', marker="x", linewidth=0.01)
+    ax.scatter(X, Y, Z, linewidth=0.01, s=1)
+
+    plt.show()
+
+def plot_bezier_tangent(material):
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    B_ys, B_r = seg_data_dir(material, 0.6)
+
+    thetas = B_ys[:,0]
+    ys = B_ys[:,1]
+    rval = B_r[:,1]
+
+    n_data = len(B_ys)
+    n_pt = 100
+
+    B = np.empty((0,3))
+    P = np.empty((0,3))
+    mus = np.empty((0,3))
+    mue = np.empty((0,3))
+    ns = np.empty((0,3))
+    dgs = np.empty((0,3))
+    ws = np.empty((0,3))
+    v = np.empty((0,3))
+
+    for i in range(n_data):
+
+        B_new, P_new, mus_new, mue_new, ns_new, dgs_new, ws_new, v_new = bezier_section_plane(material, thetas[i], ys[i], ys[n_data - i - 1], n_pt, rval[i], rval[n_data - i - 1])
+        B = np.concatenate((B, B_new))
+        P = np.concatenate((P, P_new))
+        mus = np.concatenate((mus, mus_new))
+        mue = np.concatenate((mue, mue_new))
+        ns = np.concatenate((ns, ns_new))
+        dgs = np.concatenate((dgs, dgs_new))
+        ws = np.concatenate((ws, ws_new))
+        v_new = np.expand_dims(v_new, axis=0)
+        v = np.concatenate((v, v_new))
+
+    X = B[:,0]
+    Y = B[:,1]
+    Z = B[:,2]
+
+    mus = P + mus  
+    ns = P + ns
+    dgs = P + dgs
+    ws = P + ws
+    k = 1
+    ax.scatter(X[k * 600:(k+1) * 600], Y[k * 600:(k+1) * 600], Z[k * 600:(k+1) * 600], cmap='viridis', marker="o",s=1, linewidth=1)
+
+    for i in range(k * 6, (k+1) * 6):
+        x = [P[i, 0], mus[i, 0]]
+        y = [P[i, 1], mus[i, 1]]
+        z = [P[i, 2], mus[i, 2]]
+        ax.plot(x, y, z, marker='o', markersize=1)
+
+    for i in range(k * 6, (k+1) * 6):
+        x = [P[i, 0], ns[i, 0]]
+        y = [P[i, 1], ns[i, 1]]
+        z = [P[i, 2], ns[i, 2]]
+        #ax.plot(x, y, z, marker='o', color="green")
+
+    for i in range(k * 6, (k+1) * 6):
+        x = [P[i, 0], ws[i, 0]]
+        y = [P[i, 1], ws[i, 1]]
+        z = [P[i, 2], ws[i, 2]]
+        #ax.plot(x, y, z, marker='o', color="red")
+
+    for i in range(k * 6, (k+1) * 6):
+        x = [P[i, 0], dgs[i, 0]]
+        y = [P[i, 1], dgs[i, 1]]
+        z = [P[i, 2], dgs[i, 2]]
+        #ax.plot(x, y, z, marker='o', color="blue")
+
+    Pe = np.roll(P, -1, axis=0)
+    mue = Pe + mue
+
+    for i in range(k * 6, (k+1) * 6):
+        x = [Pe[i, 0], mue[i, 0]]
+        y = [Pe[i, 1], mue[i, 1]]
+        z = [Pe[i, 2], mue[i, 2]]
+        #ax.plot(x, y, z, marker='o')
+    normal_vector = v[k]
+    point = np.array([0, 0, 0])
+
+    # Create the figure and 3D axis
+
+    # Define the plane equation ax + by + cz = d
+    d = np.dot(normal_vector, point)
+
+    # Create a grid of x, y values
+    xx, yy = np.meshgrid(range(-2, 2), range(-2, 2))
+
+    # Calculate the corresponding z values
+    a, b, c = normal_vector[0], normal_vector[1], normal_vector[2]
+    zz = (d - a * xx - b * yy) / c
+
+    # Plot the plane
+    ax.plot_surface(xx, yy, zz, alpha=0.5, rstride=100, cstride=100)
+
     plt.show()
 
 material = "DP780"
-plot_bezier(material)
+plot_bezier_tangent(material)
